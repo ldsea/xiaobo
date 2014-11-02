104eb6636aa2b1a0da5c93e60c6f6d9a764ba552
ildsea <i@ildsea.com>
2014-11-02 17:03:26 +0800

    1

---
+++ vacation/vacation/AddVacationViewController.swift
@@ -0,0 +1,36 @@
+//
+//  AddVacationViewController.swift
+//  vacation
+//
+//  Created by 海蓝 on 14/10/26.
+//  Copyright (c) 2014年 ildsea. All rights reserved.
+//
+
+import UIKit
+
+class AddVacationViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+
+}
---
+++ vacation/vacation/Vacation.swift
@@ -0,0 +1,14 @@
+//
+//  Vacation.swift
+//  vacation
+//
+//  Created by 海蓝 on 14/10/26.
+//  Copyright (c) 2014年 ildsea. All rights reserved.
+//
+
+import Foundation
+
+class Vacation{
+    var place = ""
+    var visited = false
+}
